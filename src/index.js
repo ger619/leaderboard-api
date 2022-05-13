00bc7335a8a744cd918a2eb61e24ef36d4165618
David Abol Ger <draker74@gmail.com>
2022-05-14 02:40:11 +0300

    Leader Board API

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 // css files here
 import './style.css';
+import dummy from './modules/dummy.js';
+import mockScores from './modules/mockup.js';
+
+mockScores(dummy);
+
+const container = document.querySelector('.form');
+const iName = document.querySelector('#iNname');
+const iScore = document.querySelector('#iScore');
+
+container.onsubmit = (e) => {
+  e.preventDefault();
+  const name = iName.value;
+  const score = iScore.value;
+
+  dummy.push({ name, score });
+
+  mockScores(dummy);
+};
